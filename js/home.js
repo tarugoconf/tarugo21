38d1f5a59e4a84e5738e25c8e6e2a237f82db0a3
Oscar Otero <oom@oscarotero.com>
2021-05-25 01:25:59 +0200

    speakers

--- js/home.js
+++ js/home.js
@@ -1,5 +1,3 @@
-import Player from "./vimeo-player.es.js";
-
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".navbar-menu");
 let currentLink;
@@ -53,23 +51,3 @@ menu.addEventListener("click", (event) => {
 });
 document.body.addEventListener("click", () => menu.classList.remove("is-open"));
 
-//Videos vimeo
-const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--color-primary');
-const color = primaryColor.replace('#', '').trim();
-
-//Vimeo player
-document.querySelectorAll('.video').forEach((el) => {
-  el.addEventListener("click", () => {
-    el.innerHTML = "";
-    el.classList.add("is-loaded");
-    new Player(el.id, {
-      id: el.dataset.id,
-      autoplay: true,
-      title: false,
-      byline: false,
-      portrait: false,
-      dnt: true,
-      color,
-    })
-  }, { once: true })
-});
